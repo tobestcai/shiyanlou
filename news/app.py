f065ac69b36ac07b4effd0797d83d834d762fb9d
tobestme <tobestme@126.com>
2017-12-24 11:42:30 +0800

    challange6

---
+++ news/app.py
@@ -0,0 +1,36 @@
+import os
+import json
+from flask import Flask,render_template,abort
+app = Flask(__name__)
+class Files(object):
+    def __init__(self):
+        self._files = self._read_all_files()
+    def _read_all_files(self):
+        directory = '/home/shiyanlou/files'
+        result={}
+        for filename in os.listdir(directory):
+            filepath = os.path.join(directory,filename)
+            with open(filepath) as f:
+                result[filename[:-5]] = json.load(f)
+        return result
+    def get_title_list(self):
+        return [item['title'] for item in self._files.values()]
+    def get_by_filename(self,filename):
+            return self._files.get(filename)
+
+files = Files()
+
+@app.route('/')
+def index():
+    return render_template('index.html',title_list=files.get_title_list())
+@app.route('/files/<filename>')
+def file(filename):
+    file_item = files.get_by_filename(filename)
+    if not file_item:
+        abort(404)
+    return render_template('file.html',file_item=file_item)
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
+if __name__ == '__main__':
+    app.run()
